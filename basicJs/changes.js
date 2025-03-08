21a800580c8fd79bfe1193a46baf054767ede2b5
hardik <hardik.upadhyay@qualhon.com>
2025-03-08 09:50:27 +0530

    day 1

---
+++ basicJs/changes.js
@@ -0,0 +1,7 @@
+let gameName="spiderman"
+gameName="ironman"
+console.log(gameName);
+const userName="hardik"
+userName="upadhyay"
+console.log(userName);
+
---
+++ basicJs/datatype.js
@@ -0,0 +1,22 @@
+/*
+>>>>>>>>Data Type
+1-String,
+2-Number,
+3-Boolean,
+4-BigInt,
+5-null
+6-undefined
+7-Object
+8-Symbol
+*/
+
+let score=102 // Number
+let name="hardik" // Strings
+let isLogin=true
+
+//>>>>>>>>>>>>>.Objects<<<<<<<<
+let teaType=["lemontea","orange"]
+let user ={
+    name:"hardik",
+    job:"software developer"
+}
