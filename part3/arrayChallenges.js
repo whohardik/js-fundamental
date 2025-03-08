b8ffd5d4ab51a21cbb83e6529e19ecda27cd17f3
hardik <hardik.upadhyay@qualhon.com>
2025-03-08 22:52:52 +0530

    array challenges

---
+++ part3/arrayChallenges.js
@@ -0,0 +1,36 @@
+/*
+1. Declare an array named `teaFlavors` that contain some data,
+pick first element form then save into new variable
+*/
+
+const teaFlavors=["green tea","red tea","milk tea","rose tea"]
+const firstTea=teaFlavors[0]
+console.log("🚀 ~ firstTea:", firstTea)
+
+/*
+let i have array name tea types containing "herbal tea", white tea , masala tea 
+changed second name to jasmine tea
+*/
+const teaTaste=["green tea","red tea","milk tea","rose tea"]
+teaTaste[2]="jasmine tea"
+
+// console.log(teaTaste);
+
+/*
+merged two array
+*/
+const teaTastwe=["green tea","red tea","milk tea","rose tea"]
+//const newArray=["lucknow","jvjhf"]
+const result=teaFlavors.concat(newArray);
+// console.log("🚀 ~ result:", result)
+
+/*
+take array in find there any lucknow word if yes then store in different variable 
+*/
+
+const newArray=["lucknow","jvjhf"]
+
+if (newArray.find("lucknow")) {
+    
+}
+
---
+++ part3/conditionalCheks.js
@@ -0,0 +1,18 @@
+// condition to check one number to anythore number
+
+let num1=8;
+let num2 =10
+if (num1>num2) {
+    console.log( `chota hai tu`);
+    return
+}if (num1<num2) {
+console.log(`bada hai tu`);
+return
+} if (num1==num2) {
+ console.log(`baraber hai tu`);
+ return
+} else {
+    console.log( `khuch gadbad hai daya`);
+    return
+   
+}
